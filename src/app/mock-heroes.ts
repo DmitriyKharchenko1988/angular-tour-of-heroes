285eef73ae631133e94682e4561ea8bfddcebc46
Dmitriy Kharchenko <dm.kharchenko1988@gmail.com>
2023-09-08 11:06:44 +0300

    Hero editor upd

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 12, name: 'Dr.Nice'},
+  {id: 13, name: 'Bombasto'},
+  {id: 14, name: 'Spider-man'},
+  {id: 15, name: 'Batman'},
+  {id: 16, name: 'Iron-man'},
+  {id: 17, name: 'Terminator'},
+  {id: 18, name: 'Magneta'},
+  {id: 19, name: 'Dr.Iq'},
+  {id: 20, name: 'Split'}
+];
