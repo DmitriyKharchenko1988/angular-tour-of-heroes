35b15e23ed9c57f5ff05b3fdeeb893aa3f70efa1
Dmitriy Kharchenko <dm.kharchenko1988@gmail.com>
2023-09-09 08:58:40 +0300

    add navigation

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,12 +2,12 @@ import {Hero} from './hero';
 
 export const HEROES: Hero[] = [
   {id: 12, name: 'Dr.Nice'},
-  {id: 13, name: 'Bombasto'},
+  {id: 13, name: 'Bombast'},
   {id: 14, name: 'Spider-man'},
   {id: 15, name: 'Batman'},
   {id: 16, name: 'Iron-man'},
   {id: 17, name: 'Terminator'},
-  {id: 18, name: 'Magneta'},
+  {id: 18, name: 'Magnet'},
   {id: 19, name: 'Dr.Iq'},
   {id: 20, name: 'Split'}
 ];
